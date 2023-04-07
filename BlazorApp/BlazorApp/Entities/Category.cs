1a9cc68f226acce546970bd7659a2e0a854561ec
RoblesJP <juan.robles1417@gmail.com>
2023-04-06 22:24:33 -0300

    First Commit

---
+++ BlazorApp/BlazorApp/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorApp.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
